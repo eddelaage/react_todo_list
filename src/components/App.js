615a5b94770aaaa4702379b400ff347e08ebc28b
Edouard de Laage <edouarddelaage@gmail.com>
2018-10-01 10:40:13 +0200

    Delet src folder start from the beggining

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class App extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Application TODO</h1>
+                <h2>Ajouter une todo</h2>
+                <h2>Liste des todos</h2>
+            </div>
+        )
+    }
+}
+
+export default App
