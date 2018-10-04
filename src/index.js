9415732ba527b2e324e7e4f9c46c90fa9c2d1c4e
Edouard de Laage <edouarddelaage@gmail.com>
2018-10-04 14:54:19 +0200

    Add a store in index.js and add a first reducer addTodoReducer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import React from 'react'
 import  ReactDom from 'react-dom'
+import { createStore, combineReducers } from 'redux'
 
 import App from './components/App'
+
+const addTodoReducer = (state = [], action) => {
+    switch(action.type) {
+        case('ADD_TOTO'):
+            console.log('ADD_TOTO')
+            console.log('Action', action)
+            action.payload.id = Date.now()
+            const newTodos = [...state, action.payload]
+            return newTodos
+        default:
+            return state
+    }
+}
+
+const store = createStore(combineReducers({
+    todos: addTodoReducer
+}))
 
 ReactDom.render(<App />, document.getElementById('root'))
